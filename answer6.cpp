9053ab8ff99abcd1002901e53325dabc81ac4a91
Gaurav Tewari <gauravtewari111@gmail.com>
2020-07-25 15:44:27 +0530

    answer4 added

---
+++ answer6.cpp
@@ -0,0 +1,34 @@
+//Check whether a number is Prime.
+#include <iostream>
+#include <string.h>
+using namespace std;
+void findPrime(int n)
+{
+    int count;
+ for(int i=2;i<n;i++)
+ {
+     if(n%i==0)
+     {
+         count++;
+     }
+ }
+ if(!count)
+ {
+     cout<<"It is prime";
+ }else{
+     cout<<"No, it is not a prime";
+ }
+}
+
+
+
+
+int main()
+{
+   
+  int n;
+  cin>>n;
+  findPrime(n);
+  
+    return 0;
+}
